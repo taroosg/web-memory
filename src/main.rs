db6620f605b2adaba9c10a7977b59839fbaf397f
taroosg <root@taroosg.dev>
2021-11-18 14:42:16 +0900

    add nogood templete

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,45 @@
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Error, Request, Response, Server};
-use std::{convert::Infallible, net::SocketAddr};
+use hyper::{Body, Error, Method, Request, Response, Server};
+use std::{convert::Infallible, net::SocketAddr, str};
+use tera::{Context, Tera};
+// 自作テンプレートの定義
+static TEMPLATE: &str = "Hello, {{name}}!";
 
+// リクエストに対して固定文字列のレスポンスを返す関数
 async fn handle(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-  Ok(Response::new("Hello World\n".into()))
+  Ok(Response::new("Hello World".into()))
+}
+
+// テンプレートを使用してリクエストの文字列をレスポンスに組み込む関数
+async fn handle_with_body(req: Request<Body>) -> Result<Response<Body>, Error> {
+  // bodyからバイト列のみを抽出する．
+  let body = hyper::body::to_bytes(req.into_body()).await?;
+  // バイト列を文字列として解釈する（参照のみ）．
+  let body = str::from_utf8(&body).unwrap();
+  // name=の部分を指定して抽出する（参照のみ）．
+  let name = body.strip_prefix("name=").unwrap();
+
+  // 良くない感じにテンプレートでレスポンスを構成する．
+  // 新規テンプレートの作成
+  let mut tera = Tera::default();
+  // helloという名前で定義したテンプレートを呼び出す
+  tera.add_raw_template("hello", TEMPLATE).unwrap();
+  // 新規コンテキストの作成
+  let mut ctx = Context::new();
+  // コンテキストにnameという名前でリクエストボディのnameの値を入れる
+  ctx.insert("name", name);
+  // helloテンプレートにコンテキストを適用する
+  let rendered = tera.render("hello", &ctx).unwrap();
+  // レスポンスにテンプレートを使用する
+  Ok(Response::new(rendered.into()))
 }
 
 async fn route(req: Request<Body>) -> Result<Response<Body>, Error> {
-  handle(req).await.map_err(|e| match e {})
+  match *req.method() {
+    Method::POST => handle_with_body(req).await,
+    // 固定文字列のレスポンスを返す関数を実行
+    _ => handle(req).await.map_err(|e| match e {}),
+  }
 }
 
 #[tokio::main]
